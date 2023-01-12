e1f0535e86191dc51702a167c9d09d969fcbdb65
Crusher95574 <202111026@diu.iiitvadodara.ac.in>
2023-01-12 15:56:33 +0530

    Vedio 49 completed

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt=require('bcryptjs');
+var jwt=require('jsonwebtoken');
+const JWT_SECRET ='Divayisthebest';
 
 // Create a User using: POST "/api/auth/createuser". Does't require login
 router.post(
@@ -28,14 +31,23 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this emal already exists" });
       }
+
+      const salt=await bcrypt.genSalt(10);
+      const secPass=await bcrypt.hash(req.body.password,salt);
+
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      //user => res.json(user)
-      //.catch(err=>console.log(err))
-      res.json({ user });
+      const data={
+        user:{
+            id:user.id
+        }
+      }
+       const authToken=jwt.sign(data,JWT_SECRET);
+       
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(400).send("Some Error occured");
